5d9877153dd0b914d5cf671c41574f0b917abbfa
gaeuna <pgaeun2@gmail.com>
2026-07-23 23:57:26 +0900

    [Passed] 중앙값 구하기 (52ms, 16MB)

---
+++ trail0/중앙값 구하기/find-the-median.py
@@ -0,0 +1,16 @@
+a, b, c = map(int,input().split())
+
+if a > b :
+    if b > c:
+        print(b)
+    elif a > c :
+        print(c)
+    else:
+        print(a)
+elif b > a:
+    if a > c:
+        print(a)
+    elif b > c:
+        print(c)
+    else:
+        print(b)
