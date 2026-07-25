2d4ef3d112006ecc4a06e0fdf63b7b72408a4c73
gaeuna <pgaeun2@gmail.com>
2026-07-25 22:09:39 +0900

    [Passed] 약수가 세 개인 수 (51ms, 16MB)

---
+++ trail0/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,16 @@
+start, end = map(int, input().split())
+
+total = 0
+
+for i in range(start, end+1):
+    cnt = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            cnt = cnt + 1
+            
+    if cnt == 3:
+        total = total + 1
+        
+    
+print(total)
+
