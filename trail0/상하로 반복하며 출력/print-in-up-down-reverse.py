46b63b2662469f447cfc91a61c1489726e3d6fde
gaeuna <pgaeun2@gmail.com>
2026-07-26 23:32:20 +0900

    [Passed] 상하로 반복하며 출력 (52ms, 16MB)

--- trail0/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ trail0/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -3,6 +3,8 @@ for i in range(n):
     for j in range(n):
         if j % 2 == 0:
             print(i + 1, end = "")
+            
         else:
             print(n - i, end = "")
+            
     print()
