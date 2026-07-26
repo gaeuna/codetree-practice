42855c6e7ad3f1fcf0e3a9a8ebe2bc1052e3718e
gaeuna <pgaeun2@gmail.com>
2026-07-26 18:48:34 +0900

    [Passed] 두 배열의 곱 (55ms, 16MB)

---
+++ trail0/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,26 @@
+matrix1 = []
+matrix2 = []
+for i in range (3):
+    row1 = list(map(int, input().split()))
+    matrix1.append(row1)
+input()
+for j in range (3):
+    row2 = list(map(int, input().split()))
+    matrix2.append(row2)
+
+matrix3 = []
+
+for x in range(3):
+    new_row = []
+    for h in range(3):
+        new_row.append(matrix1[x][h] * matrix2[x][h])
+    matrix3.append(new_row)
+
+for num in matrix3:
+    print(*num)
+
+
+
+
+
+
