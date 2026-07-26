46ccc8787a052e76d3837700f1856f4dd267d6c7
gaeuna <pgaeun2@gmail.com>
2026-07-26 22:56:07 +0900

    [Passed] 좌우로 반복하며 출력 (52ms, 16MB)

---
+++ trail0/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n):
+    row = []
+    cnt = 1
+
+    for j in range(n):
+        row.append(cnt)
+        cnt = cnt + 1
+
+    if i % 2 == 1:
+        for j in range(n-1, -1, -1):
+            print(row[j], end = "")
+    else:
+        for j in range(n):
+            print(row[j], end = "")
+    print()
